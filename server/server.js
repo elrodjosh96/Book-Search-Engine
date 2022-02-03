8e81007e97195c51c256290ce0604a184c5be513
Josh Elrod <elrodjosh96@gmail.com>
2022-02-02 20:04:20 -0500

    updates: grammar updates

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.get('*', (req, res) => {
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(` Now listening on localhost:${PORT}`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
